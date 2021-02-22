99605f688414502d84c919796f9133c9338d394e
maximomichael <maximomichaelfx@gmail.com>
2021-02-21 21:12:40 -0300

    ex37 e ex38

--- Exercice_Stage_04/ex_38.js
+++ Exercice_Stage_04/ex_38.js
@@ -1,1 +1,25 @@
-Testye
+//Soma de intervalo
+
+function Rage(start, end){
+    var value = [];
+    
+    if(start < end){
+        for(var x = start; end + 1 > x; x++){
+            value.push(x);
+        }
+    }
+    else{
+        for(var x = start; end - 1 < x; x--){
+            value.push(x);
+        }
+    }
+
+    var soma = 0;
+    for(var x = 0; x < value.length; x++){
+        soma += value[x];
+    }
+
+    return value +  " = " + soma;
+    
+}
+console.log(Rage(1, 10));
